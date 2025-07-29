a1fefc6dc2b7056b118958256a2ee6af16bfa917
Krish Gupta <m.krishggupta@icloud.com>
2025-07-30 01:25:27 +0530

    fix issue schema err

--- src/lib/config/gh-automator.ts
+++ src/lib/config/gh-automator.ts
@@ -9,14 +9,21 @@ export const issueSchema = z.object({
   id: z.string().min(1, "Program slug is required"),
   name: dataSchema.shape.name.min(1, "Program name is required"),
   type: z.array(dataSchema.shape.type).transform((v) => v[0]),
-  draft: z.array(dataSchema.shape.draft).transform((v) => v[0]),
+  draft: z
+    .array(z.string().transform((v) => dataSchema.shape.draft.parse(v)))
+    .transform((v) => v[0]),
   description: dataSchema.shape.description.min(1, "Description is required"),
   ys: dataSchema.shape.ys.min(1, "YS is required"),
   ws: dataSchema.shape.ws.min(1, "WS is required"),
   website: dataSchema.shape.extern.shape.website.optional().default(null),
   "slack-link": dataSchema.shape.extern.shape.slack.shape.link,
   "slack-name": dataSchema.shape.extern.shape.slack.shape.name,
-  timeline: dataSchema.shape.timeline,
+  timeline: z
+    .string()
+    .transform((v) => JSON.parse(v))
+    .transform((v) => {
+      return dataSchema.shape.timeline.parse(v);
+    }),
 });
 
 export type IssueData = z.infer<typeof issueSchema>;
