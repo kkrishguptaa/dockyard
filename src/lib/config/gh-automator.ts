fcaa3ff6f6b117e26c0680191b5a1443063564d7
Krish Gupta <m.krishggupta@icloud.com>
2025-07-30 01:37:12 +0530

    attempt billion

--- src/lib/config/gh-automator.ts
+++ src/lib/config/gh-automator.ts
@@ -8,28 +8,40 @@ export const gitHubIssueBaseUrl = new URL(
 export const issueSchema = z.object({
   id: z.string().min(1, "Program slug is required"),
   name: dataSchema.shape.name.min(1, "Program name is required"),
-  type: z.array(dataSchema.shape.type).transform((v) => v[0]),
+
+  type: z
+    .union([dataSchema.shape.type, z.array(dataSchema.shape.type)])
+    .transform((v) => (Array.isArray(v) ? v[0] : v)),
+
   draft: z
-    .array(
-      z
-        .string()
-        .transform((v) =>
-          dataSchema.shape.draft.parse(new Boolean(v).valueOf())
-        )
-    )
-    .transform((v) => v[0]),
+    .union([
+      dataSchema.shape.draft,
+      z.boolean(),
+      z.array(z.union([z.boolean(), z.string()])),
+    ])
+    .transform((v) => {
+      if (typeof v === "boolean") return v;
+      if (typeof v === "string")
+        return dataSchema.shape.draft.parse(v === "true");
+
+      const first = Array.isArray(v) ? v[0] : v;
+
+      return typeof first === "boolean"
+        ? first
+        : dataSchema.shape.draft.parse(first === "true");
+    }),
+
   description: dataSchema.shape.description.min(1, "Description is required"),
   ys: dataSchema.shape.ys.min(1, "YS is required"),
   ws: dataSchema.shape.ws.min(1, "WS is required"),
   website: dataSchema.shape.extern.shape.website.optional().default(null),
   "slack-link": dataSchema.shape.extern.shape.slack.shape.link,
   "slack-name": dataSchema.shape.extern.shape.slack.shape.name,
-  timeline: z
-    .string()
-    .transform((v) => JSON.parse(v))
-    .transform((v) => {
-      return dataSchema.shape.timeline.parse(v);
-    }),
+
+  timeline: z.union([z.string(), dataSchema.shape.timeline]).transform((v) => {
+    const arr = typeof v === "string" ? JSON.parse(v) : v;
+    return dataSchema.shape.timeline.parse(arr);
+  }),
 });
 
 export type IssueData = z.infer<typeof issueSchema>;
